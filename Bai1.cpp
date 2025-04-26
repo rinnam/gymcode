4ec2617122146b21d742b6454dc213cb6416a254
rinnam <rinnam0901@gmail.com>
2025-04-26 10:50:37 +0700

    nop baii

--- Bai1.cpp
+++ Bai1.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-int main() {
-    int n;
-    cin >> n;
 
-    if (n <= 2) {
-        cout << "Ko co so nguyen to" << endl;
+bool snt(int n) {
+    if (n < 2)
+        return false;
+    for (int i = 2; i <= sqrt(n); ++i) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int N;
+    cout << "Nhap so nguyen duong N: ";
+    cin >> N;
+
+    if (N <= 2) {
+        cout << "Khong co so nguyen to" << endl;
     } else {
-        cout << "cac so nguyen to nho hon " << n;
-        for (int i = 2; i < n; ++i) {
+        cout << "Cac so nguyen to nho hon " << N << " la: ";
+        for (int i = 2; i < N; ++i) {
             if (snt(i)) {
                 cout << i << " ";
             }
@@ -19,16 +31,4 @@ int main() {
 
     return 0;
 }
-
-bool snt(int n){
-	if (n<=2){
-		return false;
-	}
-	for (int i = 2; i <= sqrt(n); ++i) {
-        if (n % i == 0)
-            return false;
-    }
-    return true;
-}
-	
 
